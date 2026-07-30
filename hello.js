5205ffb0e54fa4c6e286263a0a4cab525ef513b3
global <sahadeo.shanae@gmail.com>
2026-07-30 00:30:37 -0400

    print the date

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
 let course = "Level 2";
-let message = "Hello from " + course + "!"
-console.log(message)
-console.log("This JavaScript runs on MY Machine. Powered by Shanae.")
-console.log("hello im shanae")
+
+let message = "Hello from " + course + "!";
+console.log(message);
+console.log("This JavaScript runs on MY Machine. Powered by Shanae.");
+console.log("gitHub @helloimshanae");
+
+console.log(new Date());
+

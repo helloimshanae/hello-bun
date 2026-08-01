de499b91fe6782de1a4a2ba0f7960bc19656eb9e
global <sahadeo.shanae@gmail.com>
2026-07-31 21:30:23 -0400

    Added a READ.ME & included 2 sentences about this project and what im using to learn/create it.

--- hello.js
+++ hello.js
@@ -1,9 +1,10 @@
 let course = "Level 2";
 
 let message = "Hello from " + course + "!";
-console.log(message);
-console.log("This JavaScript runs on MY Machine. Powered by Shanae.");
-console.log("gitHub @helloimshanae");
+console.log("\n")
+console.log(message)
+console.log("This JavaScript runs on MY Machine. Powered by Shanae.\n");
+console.log("gitHub @helloimshanae\n");
 
 console.log(new Date());
 
